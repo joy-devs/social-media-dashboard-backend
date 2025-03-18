b2d4bc6b21ccb83981590fa4f7197dda675a2328
Joyce Wambui <joycewambui@gmail.com>
2025-03-18 16:57:15 +0300

    made changes

--- src/tokens/tokens.services.ts
+++ src/tokens/tokens.services.ts
@@ -1,6 +1,5 @@
 import { tokens, users, TIToken, TSToken } from "../drizzle/schema";
 import db from "../drizzle/db";
-import { sql } from "drizzle-orm";
 import { eq } from "drizzle-orm";
 
 
@@ -33,17 +32,17 @@ export const deletetokensService = async (id: number): Promise<string> => {
 
 
 // User login service
-export const userLoginService = async (user: TSToken) => {
-    const { username, password } = user; // Adjust this if necessary for password hash comparison
+// export const userLoginService = async (user: TSToken) => {
+//     const { username, password } = user; // Adjust this if necessary for password hash comparison
 
-    return await db.query.users.findFirst({
-        columns: {
-            id: true,
-            username: true,
-            email: true,
-            passwordHash: true, // Check for hashed password
-            bio: true,
-        },
-        where: sql`${users.username} = ${username}`, // Using the correct field from your schema
-    });
-};
+//     return await db.query.users.findFirst({
+//         columns: {
+//             id: true,
+//             username: true,
+//             email: true,
+//             passwordHash: true, // Check for hashed password
+//             bio: true,
+//         },
+//         where: sql`${users.username} = ${username}`, // Using the correct field from your schema
+//     });
+// };
